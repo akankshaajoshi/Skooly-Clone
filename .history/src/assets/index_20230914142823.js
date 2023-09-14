2b968ddd5f18d6ee4e6b6092551c800087ad4196
Akanksha <joshiaj33@gmail.com>
2023-09-14 14:44:16 +0530

    feat(assets): added font styles and colors

---
+++ .history/src/assets/index_20230914142823.js
@@ -0,0 +1,23 @@
+const PRIMARY = "";
+const SECONDARY = "";
+const TERTIARY = "";
+const QUATERNARY =  "";
+
+export const colors = {
+    primary: PRIMARY,
+    secondary: SECONDARY,
+    tertiary: TERTIARY,
+    quaternary: QUATERNARY,
+    white: "white",
+    black: "black"
+}
+
+export const fontSizes = {
+    xSmall: "10px",
+    small: "14px",
+    medium: "20px",
+    large: "24px",
+    xLarge: "28px"
+}
+
+
