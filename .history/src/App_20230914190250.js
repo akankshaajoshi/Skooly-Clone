fe4f78ae9117f90ebd640160c7cb2e2aed491f9b
Akanksha <joshiaj33@gmail.com>
2023-09-14 19:27:06 +0530

    update(images): changed images

--- .history/src/App_20230914190250.js
+++ .history/src/App_20230914190250.js
@@ -1,5 +1,5 @@
 import "@/styles/App.css"
-//import Navbar from "@/components/shared/Navbar"
+import Navbar from "@/components/shared/Navbar"
 import HeroContainer from "@/containers/HeroContainer"
 import DownloadContainer from "@/containers/DownloadContainer"
 import ForSchoolContainer from "@/containers/ForSchoolContainer"
